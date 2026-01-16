2689fea6f68e2f7c80861c8384a300369985ecab
Ruslan Yunusbaev <ruslan.iunusbaev@tages.ru>
2026-01-16 13:08:43 +0500

    refactor config

--- config/provider.go
+++ config/provider.go
@@ -5,7 +5,8 @@ import (
 	_ "embed"
 
 	ujconfig "github.com/crossplane/upjet/v2/pkg/config"
-	"github.com/tagesjump/provider-opensearch/config/roles"
+	rolesClustered "github.com/tagesjump/provider-opensearch/config/cluster/roles"
+	namespacedClustered "github.com/tagesjump/provider-opensearch/config/namespaced/roles"
 )
 
 const (
@@ -30,7 +31,27 @@ func GetProvider() *ujconfig.Provider {
 		))
 
 	for _, configure := range []func(provider *ujconfig.Provider){
-		roles.Configure,
+		rolesClustered.Configure,
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
+
+// GetProvider returns provider configuration
+func GetProviderNamespaced() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithRootGroup("opensearch.m.upbound.io"),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithFeaturesPackage("internal/features"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		namespacedClustered.Configure,
 	} {
 		configure(pc)
 	}
